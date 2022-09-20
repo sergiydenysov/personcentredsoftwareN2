4dc801af7a4421ec46fcae29ff9eee4f22f0e52b
Alex Denysov <Alex.Denysov@in-syncgroup.com>
2022-09-21 00:59:48 +0100

    Add project files.

---
+++ personcentredsoftwareN2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace personcentredsoftwareN2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AS I am doing this excersise from memory I am asyming that in the string "4:2" 
+            //4 represennts amount og Gas on this station
+            //and 2 is represents amount of gas needed to get to this station from prevous station
+
+            var sucsesfulRunStartIndeses = new List<int>();
+
+            string[] stringArray = { "4:0", "2:1", "3:2", "5:12", "3:1" };
+            var originalList = new List<Station>();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                originalList.Add(Station.convertToStation(stringArray[i], i));
+            }
+
+            foreach (var st in originalList)
+            {
+                bool sucsesfullRun = true;
+                var list = new List<Station>();
+
+                list.AddRange(originalList.GetRange(st.Index, originalList.Count - st.Index));
+                if (st.Index != 0)
+                {
+                    list.AddRange(originalList.GetRange(0, st.Index));
+                }
+
+                var startIndex = list.First().Index;
+                var amountGas = list.First().AmountOfGasOnStaion;
+                list.Remove(list.First());
+                foreach (var item in list)
+                {
+                    amountGas = amountGas + item.AmountOfGasOnStaion - item.AmoutToGetToStaion;
+                    if (amountGas < 0)
+                    {
+                        sucsesfullRun = false;
+                    }
+                }
+
+                if (sucsesfullRun)
+                {
+                    sucsesfulRunStartIndeses.Add(startIndex);
+                }
+            }
+
+            Console.WriteLine("results " + String.Join(", ", sucsesfulRunStartIndeses.ToArray()));
+        }
+    }
+}
---
+++ personcentredsoftwareN2/Station.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace personcentredsoftwareN2
+{
+    public class Station
+    {
+        public int Index { get; set; }
+        public int AmountOfGasOnStaion { get; set; }
+        public int AmoutToGetToStaion { get; set; }
+
+        public Station(int index, int amountOfGasOnStaion, int amoutToGetToStaion)
+        {
+            Index = index;
+            AmountOfGasOnStaion = amountOfGasOnStaion;
+            AmoutToGetToStaion = amoutToGetToStaion;
+        }
+
+        public static Station convertToStation(string gassStaion, int index)
+        {
+            int amountOfGasOnStaion = 0;
+            int amoutToGetToStaion = 0;
+
+            if (gassStaion.Contains(":"))
+            {
+                string[] gassStaionNumbers = gassStaion.Split(':');
+                amountOfGasOnStaion = convertToInt(gassStaionNumbers[0]);
+                amoutToGetToStaion = convertToInt(gassStaionNumbers[1]);
+            }
+            else
+            {
+                amountOfGasOnStaion = convertToInt(gassStaion);
+            }
+
+            return new Station(index, amountOfGasOnStaion, amoutToGetToStaion);
+        }
+
+        public static int convertToInt(string st)
+        {
+            int amount = 0;
+            try
+            {
+                amount = Int32.Parse(st);
+                return amount;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please check your input data -> " + st);
+            }
+            return amount;
+        }
+    }
+}
